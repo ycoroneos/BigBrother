2e3b14df3c5edafed22d9a212f66541fc57392be
Yanni Coroneos <ycoroneo@mit.edu>
2013-03-23 17:14:38 -0400

    working network image transfer

--- client/main.cpp
+++ client/main.cpp
@@ -14,7 +14,8 @@ int main(int argc, char* argv[])
 {
     SocketClient remote("localhost", "5555");
     namedWindow("feed", 1);
-    while (1)
+    int invalid_count=0;
+    while (invalid_count<=3)
     {
         int data_size=0;
         Mat frame;
@@ -24,13 +25,16 @@ int main(int argc, char* argv[])
         if (!frame.data)
         {
           std::cout << "invalid image\n";
+          ++invalid_count;
         }
         else
         {
           imshow("feed", frame);
+          invalid_count=0;
         }
         waitKey(20);
     }
     remote.sendmessage("bye");
+    std::cout << "lost connection... probably\n";
     return 0;
 }
