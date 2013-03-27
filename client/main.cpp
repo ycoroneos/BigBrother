6438619855d3b50d6b19f623c2f4c1329a3a811e
Yanni Coroneos <ycoroneo@mit.edu>
2013-03-26 22:15:34 -0400

    added CXX flags

--- client/main.cpp
+++ client/main.cpp
@@ -12,14 +12,15 @@ using namespace cv;
 
 int main(int argc, char* argv[])
 {
-    SocketClient remote("18.111.72.199", "5555");
+    SocketClient remote("localhost", "5555");
     namedWindow("feed", 1);
     int invalid_count=0;
-    while (invalid_count<=3)
+    while (1)
     {
         int data_size=0;
         Mat frame;
         std::vector<unsigned char>buffer=remote.getRawFrameData("image");
+        std::cout << buffer.size() << std::endl;
         if (buffer.size()>0)
           frame=imdecode(Mat(buffer), CV_LOAD_IMAGE_COLOR); //Mat constructor takes std::vector<uchar>
         if (!frame.data)
